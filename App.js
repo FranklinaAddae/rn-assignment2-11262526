1530b81467dd73144ac93d8c802bd36edcf8b405
Franklina Addae <faddae011@st.ug.edu.gh>
2024-05-27 15:26:10 +0000

    Bolded my name in the Text component

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is Franklina!</Text>
+      <Text style={styles.text}>
+        My name is <Text style={styles.name}>Franklina!</Text>
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -20,5 +22,9 @@ const styles = StyleSheet.create({
 
   text: {
     fontSize: 24,
+  },
+
+  name: {
+    fontWeight: 'bold',
   }
 });
